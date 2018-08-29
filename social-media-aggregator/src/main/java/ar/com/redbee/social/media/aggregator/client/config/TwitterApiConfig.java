f5ec90413004befce81bfecbe3cf6e8b33f6f563
Unknown <wolfsdorf.ivan@gmail.com>
2018-08-29 00:29:43 -0300

    Se agrega configuracion de keys para la api de twitter

--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/client/config/TwitterApiConfig.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/client/config/TwitterApiConfig.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Component;
 public class TwitterApiConfig {
 
 	private String url;
+	private String customerKey;
+	private String customerSecret;
+	private String tokenKey;
+	private String tokenSecret;
 
 	public String getUrl() {
 		return url;
@@ -15,5 +19,37 @@ public class TwitterApiConfig {
 
 	public void setUrl(final String url) {
 		this.url = url;
+	}
+
+	public String getCustomerKey() {
+		return customerKey;
+	}
+
+	public void setCustomerKey(final String customerKey) {
+		this.customerKey = customerKey;
+	}
+
+	public String getCustomerSecret() {
+		return customerSecret;
+	}
+
+	public void setCustomerSecret(final String customerSecret) {
+		this.customerSecret = customerSecret;
+	}
+
+	public String getTokenKey() {
+		return tokenKey;
+	}
+
+	public void setTokenKey(final String tokenKey) {
+		this.tokenKey = tokenKey;
+	}
+
+	public String getTokenSecret() {
+		return tokenSecret;
+	}
+
+	public void setTokenSecret(final String tokenSecret) {
+		this.tokenSecret = tokenSecret;
 	}
 }
