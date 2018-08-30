3b9775e4cfcad23c5cfa8ad1cc5a1cb4e8d816ee
Unknown <wolfsdorf.ivan@gmail.com>
2018-08-30 00:04:34 -0300

    Se modifica objeto devuelto desde el userDao

--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/dao/UserDao.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/dao/UserDao.java
@@ -1,11 +1,13 @@
 package ar.com.redbee.social.media.aggregator.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ar.com.redbee.social.media.aggregator.model.User;
 
 public interface UserDao extends CrudRepository<User, Long> {
 
-  User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 
 }
