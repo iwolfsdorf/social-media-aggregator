60a064e782abed621b3894ad2c00c585d372f2c7
Unknown <wolfsdorf.ivan@gmail.com>
2018-08-30 15:52:27 -0300

    Se elimina metodo sin uso

--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/service/impl/BoardServiceImpl.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/service/impl/BoardServiceImpl.java
@@ -1,45 +1,34 @@
 package ar.com.redbee.social.media.aggregator.service.impl;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 import ar.com.redbee.social.media.aggregator.dao.BoardDao;
 import ar.com.redbee.social.media.aggregator.exception.ServiceException;
 import ar.com.redbee.social.media.aggregator.model.Board;
-import ar.com.redbee.social.media.aggregator.model.User;
 import ar.com.redbee.social.media.aggregator.service.BoardService;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
-	@Autowired
-	private BoardDao boardDao;
+  @Autowired
+  private BoardDao boardDao;
 
-	@Override
-	public void save(final Board board) throws ServiceException {
-		boardDao.save(board);
-	}
+  @Override
+  public void save(final Board board) throws ServiceException {
+    boardDao.save(board);
+  }
 
-	@Override
-	public Board get(final Long id) throws ServiceException {
-		final Optional<Board> optionalBoard = boardDao.findById(id);
-		if (optionalBoard.isPresent()) {
-			return optionalBoard.get();
-		} else {
-			throw new ServiceException("No existe el board con id " + id);
-		}
-	}
-
-	@Override
-	public Board getByUserAndDescription(final User user, final String description) throws ServiceException {
-		final Optional<Board> optionalBoard = boardDao.findByUserAndDescription(user, description);
-		if (optionalBoard.isPresent()) {
-			return optionalBoard.get();
-		} else {
-			throw new ServiceException("No existe el board con usuario " + user.getId() + " y descripcion " + description);
-		}
-	}
+  @Override
+  public Board get(final Long id) throws ServiceException {
+    final Optional<Board> optionalBoard = boardDao.findById(id);
+    if (optionalBoard.isPresent()) {
+      return optionalBoard.get();
+    } else {
+      throw new ServiceException("No existe el board con id " + id);
+    }
+  }
 
 }
