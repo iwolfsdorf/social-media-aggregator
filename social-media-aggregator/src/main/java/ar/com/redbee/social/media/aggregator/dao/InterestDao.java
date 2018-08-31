c0303e4eb3611bf59ff6ca4f2b155086b515d8a9
Unknown <wolfsdorf.ivan@gmail.com>
2018-08-31 14:33:26 -0300

    Se crea Dao de interest

---
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/dao/InterestDao.java
@@ -0,0 +1,9 @@
+package ar.com.redbee.social.media.aggregator.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.com.redbee.social.media.aggregator.model.Interest;
+
+public interface InterestDao extends CrudRepository<Interest, Long> {
+
+}
