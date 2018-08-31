d063d3ba563a449cc7ce503211591d76be14e507
Unknown <wolfsdorf.ivan@gmail.com>
2018-08-31 14:30:46 -0300

    Se modifica retorno al buscar los tweets

--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/client/TwitterApiClient.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/client/TwitterApiClient.java
@@ -1,7 +1,11 @@
 package ar.com.redbee.social.media.aggregator.client;
 
+import java.util.List;
+
+import ar.com.redbee.social.media.aggregator.model.Tweet;
+
 public interface TwitterApiClient {
 
-	public String search(String query);
+  public List<Tweet> search(String query);
 
 }
--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/client/impl/TwitterApiClientImpl.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/client/impl/TwitterApiClientImpl.java
@@ -1,5 +1,8 @@
 package ar.com.redbee.social.media.aggregator.client.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +12,22 @@ import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Service;
 
 import ar.com.redbee.social.media.aggregator.client.TwitterApiClient;
+import ar.com.redbee.social.media.aggregator.model.Tweet;
 
 @Service
 public class TwitterApiClientImpl implements TwitterApiClient {
 
-	private static final Logger log = LoggerFactory.getLogger(TwitterApiClientImpl.class);
+  private static final Logger log = LoggerFactory.getLogger(TwitterApiClientImpl.class);
 
-	@Autowired
-	private TwitterTemplate twitterTemplate;
+  @Autowired
+  private TwitterTemplate twitterTemplate;
 
-	@Override
-	public String search(final String query) {
-		log.info("Se realiza consulta de twitters con la query {}.", query);
-		SearchParameters searchP = new SearchParameters(query);
-		final SearchResults search = twitterTemplate.searchOperations().search(searchP);
-		return search.getTweets().toString();
-	}
+  @Override
+  public List<Tweet> search(final String query) {
+    log.info("Se realiza consulta de twitters con la query {}.", query);
+    final SearchParameters searchP = new SearchParameters(query);
+    final SearchResults search = twitterTemplate.searchOperations().search(searchP);
+    return search.getTweets().stream().map(t -> Tweet.toTweet(t)).collect(Collectors.toList());
+  }
 
 }
