8ec8ab04c9bb626b791a91d9fb05e646b5f63246
Unknown <wolfsdorf.ivan@gmail.com>
2018-08-31 14:35:47 -0300

    Se agrega metodo getAll

--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/service/BoardService.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/service/BoardService.java
@@ -1,5 +1,7 @@
 package ar.com.redbee.social.media.aggregator.service;
 
+import java.util.List;
+
 import ar.com.redbee.social.media.aggregator.exception.ServiceException;
 import ar.com.redbee.social.media.aggregator.model.Board;
 
@@ -8,5 +10,7 @@ public interface BoardService {
   public void save(Board board) throws ServiceException;
 
   public Board get(Long id) throws ServiceException;
+
+  public List<Board> getAll() throws ServiceException;
 
 }
--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/service/impl/BoardServiceImpl.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/service/impl/BoardServiceImpl.java
@@ -1,9 +1,11 @@
 package ar.com.redbee.social.media.aggregator.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 import ar.com.redbee.social.media.aggregator.dao.BoardDao;
 import ar.com.redbee.social.media.aggregator.exception.ServiceException;
@@ -29,6 +31,15 @@ public class BoardServiceImpl implements BoardService {
     } else {
       throw new ServiceException("No existe el board con id " + id);
     }
+  }
+
+  @Override
+  public List<Board> getAll() throws ServiceException {
+    final List<Board> boards = new ArrayList<>();
+    for (final Board board : boardDao.findAll()) {
+      boards.add(board);
+    }
+    return boards;
   }
 
 }
