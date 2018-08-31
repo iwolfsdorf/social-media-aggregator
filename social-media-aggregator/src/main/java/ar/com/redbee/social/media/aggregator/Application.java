6fe00ef135559ee70d67ba24f473b7c4362e2bda
Unknown <wolfsdorf.ivan@gmail.com>
2018-08-31 14:29:46 -0300

    Se habilita Schedul para crear jobs

--- social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/Application.java
+++ social-media-aggregator/src/main/java/ar/com/redbee/social/media/aggregator/Application.java
@@ -2,8 +2,10 @@ package ar.com.redbee.social.media.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"ar.com.redbee"})
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = { "ar.com.redbee" })
 public class Application {
 
   public static void main(final String[] args) {
